0f4fe65f75ee54fc33ecfbbd0876e1ff473a3a2b
sameberenz <samweli@posteo.de>
2020-02-03 11:27:58 +0100

    remove TIFFs in results and update world map plot for LitPop

--- 201903_litpop_exposure_data_model/climada_v1.3.1/litpop_world_map_3.py
+++ 201903_litpop_exposure_data_model/climada_v1.3.1/litpop_world_map_3.py
@@ -22,8 +22,8 @@ from climada.entity import Exposures
 version_date = 'v1_1'
 REF_YEAR = 2014
 RES_ARCSEC = 30
-res_target = 1800
-res_targets = [1800, 600]
+
+res_targets = [600] #[1800, 600]
 ENTITY_DIR = os.path.join(SYSTEM_DIR, 'litpop_%i' % (REF_YEAR))
 ENTITY_DIR_HDF5 = os.path.join(SYSTEM_DIR, 'litpop_%i_hdf5' % (REF_YEAR))
 filename_start = 'LitPop_pc_%iarcsec_' % (RES_ARCSEC)
@@ -31,9 +31,13 @@ RES_DIR = '/Users/eberenzs/Documents/Projects/climada_papers/201903_litpop_expos
 
 write_to_hdf5 = False
 try_read_from_hdf5 = False
-write_to_tiff = True
+write_to_tiff = False
 try_read_from_tiff = True
 save_plots = True
+
+fix_zeros = True # CHANGES TOTAL VALUES
+
+plot_minimum = 100
 
 if not os.path.exists(RES_DIR):
     os.makedirs(RES_DIR)
@@ -80,6 +84,8 @@ for res_target in res_targets:
             if write_to_hdf5:
                 exposure_tmp.write_hdf5(os.path.join(ENTITY_DIR_HDF5, '%s.hdf5' %(fi[0:-4])))
             if write_to_tiff:
+                if fix_zeros:
+                    exposure_tmp.value[exposure_tmp.value<1] = 1
                 # exposure_tmp.plot_raster(res=RES_ARCSEC/3600, save_tiff=\
                 #                          os.path.join(RES_DIR, '%s_%ias.tiff' %(fi[0:-4], RES_ARCSEC)))
                 exposure_tmp.plot_raster(res=RES_ARCSEC/3600, raster_res=res_target/3600, save_tiff=\
@@ -97,6 +103,8 @@ for res_target in res_targets:
             exposure_tmp.set_geometry_points() # set geometry attribute (shapely Points) from GeoDataFrame from latitude and longitude
             exposure_tmp.check() # puts metadata that has not been assigned
             exposure_data = exposure_data.append(exposure_tmp)
+            if if fix_zeros:
+                exposure_tmp.value[exposure_tmp.value<plot_minimum] = plot_minimum
         else:
             print('\n' + '\x1b[1;03;30;30m' + 'ERROR Loading: %s_%ias.tiff' %(fi[0:-4], res_target) + '\x1b[0m')
         
@@ -106,7 +114,7 @@ for res_target in res_targets:
         print('\n' + '\x1b[1;03;30;30m' + 'Writing combined data to TIFF...' + '\x1b[0m')
         exposure_data.plot_raster(res=res_target/3600, raster_res=res_target/3600, save_tiff=\
             os.path.join(RES_DIR, '%s_000_%ias.tiff' %(files[0][0:-8], res_target)))
-    ax_exp = exposure_data.plot_hexbin(pop_name=False, cmap='plasma', norm=LogNorm(vmin=10, vmax=0.1*exposure_data.value.max()))
+    ax_exp = exposure_data.plot_hexbin(pop_name=False, cmap='plasma', norm=LogNorm(vmin=plot_minimum, vmax=0.1*exposure_data.value.max()))
     if save_plots:
         plt.savefig(os.path.join(RES_DIR, 'LitPop_pc_%iarcsec_%i_world_map.png' % (res_target, REF_YEAR)), \
                       dpi=600, facecolor='w', edgecolor='w', \
@@ -121,7 +129,7 @@ for res_target in res_targets:
     
     
     ax_exp_scatter = exposure_data.plot_scatter(pop_name=False, cmap='plasma', s=.01, shapes=False, \
-                                                norm=LogNorm(vmin=100, vmax=0.1*exposure_data.value.max()))
+                                                norm=LogNorm(vmin=plot_minimum, vmax=0.1*exposure_data.value.max()))
 
     shp_file = shapereader.natural_earth(resolution='10m', \
                     category='cultural', name='admin_0_countries')
@@ -131,13 +139,13 @@ for res_target in res_targets:
                                 edgecolor='black', linewidth=.5)
     
     if save_plots:
-        plt.savefig(os.path.join(RES_DIR, 'LitPop_pc_%iarcsec_%i_world_map_scatter001pt_lw.png' % (res_target, REF_YEAR)), \
+        plt.savefig(os.path.join(RES_DIR, 'LitPop_pc_%iarcsec_%i_world_map_scatter001pt_lw3.png' % (res_target, REF_YEAR)), \
                       dpi=300, facecolor='w', edgecolor='w', \
                       orientation='portrait', papertype=None, format='png', \
                       transparent=False, bbox_inches=None, pad_inches=0.1, \
                       frameon=None, metadata=None)
-        plt.savefig(os.path.join(RES_DIR, 'LitPop_pc_%iarcsec_%i_world_map_scatter001pt_lw.pdf' % (res_target, REF_YEAR)), \
-                      dpi=600, facecolor='w', edgecolor='w', \
+        plt.savefig(os.path.join(RES_DIR, 'LitPop_pc_%iarcsec_%i_world_map_scatter001pt_lw3.pdf' % (res_target, REF_YEAR)), \
+                      dpi=300, facecolor='w', edgecolor='w', \
                       orientation='portrait', papertype=None, format='pdf', \
                       transparent=False, bbox_inches=None, pad_inches=0.1, \
                           frameon=None, metadata=None)
